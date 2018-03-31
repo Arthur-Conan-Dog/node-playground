0d005e6e09b092f9edfcf67dadac43b9493b678c
Arthur Shaw <arthur.shaw.official@gmail.com>
2018-03-31 11:52:46 +0800

    query using mongoose

---
+++ playground/query.js
@@ -0,0 +1,18 @@
+const { mongoose } = require('../server/db/mongoose')
+const { Todo } = require('../server/models/todo')
+
+const { ObjectID } = require('mongodb')
+
+// const id = new ObjectID('5abdf9cf4f9e01344044e238')
+const id = '5abefa1c4048475b3d579884'
+
+if (!ObjectID.isValid(id)) {
+  console.error('Not valid id')
+}
+
+// Todo.find({ _id: id }).then(res => console.log(res))
+
+// Todo.findOne({ _id: id }).then(res => console.log(res))
+
+Todo.findById(id).then(res => console.log(res)).catch(e => console.error(e))
+
